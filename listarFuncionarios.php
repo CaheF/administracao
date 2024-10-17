d19c0d9ab8581fad722813c43bc7f58966a7bc00
cahe <diegoolsv714@gmail.com>
2024-10-17 16:00:42 -0300

    pronto

--- listarFuncionarios.php
+++ listarFuncionarios.php
@@ -1,7 +1,41 @@
 <?php
+    include('back/conectar.php');
     include('menu.php');
 
+    echo "<div class='tabela'>";
     $sql = "SELECT * FROM funcionarios";
+    $result = $conn->query($sql);
+
+    if($result->num_rows > 0) {
+        echo  "<table border ='1'>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>Data de nascimento</th>
+                    <th>Documento</th>
+                    <th>Salario</th>     
+                </tr>";
+
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr> 
+                <td>" .
+                $row["idFuncionario"] . "</td>
+                <td>" . 
+                $row["nome"] . "</td>
+                <td>" . 
+                $row["dataNasc"] . "</td>
+                <td>" . 
+                $row["documento"] . "</td>
+                <td>" . 
+                $row["salario"] . "</td> 
+        </td>";
+    }
+    echo "</table>";
+} else {
+    echo "Nenhum funcionario encontrado.";
+}
+echo "</div>";
+$conn->close();
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +45,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Funcionarios</title>
     <link rel="stylesheet" href="CSS/menu.css">
+    <link rel="stylesheet" href="CSS/listFun.css">
 </head>
 <body>
     
