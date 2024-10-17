87c3d4170efb7c19eaf22b8d243fbcccc2c5b61d
Cahe <cahefelipe10@gmail.com>
2024-10-16 22:51:04 -0300

    alterações no css

--- menu.php
+++ menu.php
@@ -2,5 +2,7 @@
     <a href="calPLR.php">Cálculo de PLR</a>
     <a href="cadFuncionario.php">Cadastro de Funcionários</a>
     <a href="cadFalta.php">Registrar Falta</a>
+    <a href="cadDep.php">Cadastrar departamento</a>
+    <a href="cadFolga.php">Registrar Folga</a>
     <a href="listarFuncionarios.php">Listar Funcionários</a>
 </div>
